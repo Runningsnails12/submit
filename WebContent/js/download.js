a18f25630abb87f50ebc7cf6d4cf6de7bc307755
60rzvvbj <1808078515@qq.com>
2020-12-22 10:25:01 +0800

    新增下载页面

---
+++ WebContent/js/download.js
@@ -0,0 +1,25 @@
+var body = getDom('body');
+function run(path) {
+    var urlArr = new Array();
+    for (var i = 0; i < x.length; i++) {
+        urlArr.push(path[i].split('\\')[3]);
+    }
+    urlArr.sort();
+    console.log(urlArr);
+    for (var i = 0; i < urlArr.length; i++) {
+        var a = document.createElement('a');
+        var url = urlArr[i];
+        console.log(url);
+        a.href = 'http://106.15.94.131/shaobing/io/downloadd?filename=' + url;
+        a.innerText = url;
+        a.style.display = 'block';
+        body.appendChild(a);
+    }
+}
+ajax({
+    type: 'get',
+    url: '/shaobing/io/getPath',
+    success: function (res) {
+        run(res);
+    }
+});
